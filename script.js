f57527a097c753d7c011f499161e71c443cd52d0
Nishta <nishtak2@illinois.edu>
2024-06-01 17:16:32 +0530

    phone display for home page

--- script.js
+++ script.js
@@ -18,3 +18,54 @@ function updateDimensions() {
     linkIcon.style.left = 22 + "vw"
     linkIcon.style.width = 3 + "vw"
 }
+
+window.addEventListener("load", updateDimensions);
+window.addEventListener("resize", updateDimensions);
+
+function updateStylesForPhone() {
+    if (window.matchMedia("(max-width: 480px)").matches) {
+        var mailIcon = document.querySelector(".mail-icon");
+        var gitIcon = document.querySelector(".github-icon");
+        var linkedinIcon = document.querySelector(".linkedin-icon");
+        var sideblock = document.querySelector(".sideblock");
+        var sideblockBottom = sideblock.getBoundingClientRect().bottom;
+
+        mailIcon.style.top = (sideblockBottom+20) + "px";
+        mailIcon.style.left = 22 + "vw"
+        mailIcon.style.width = 12 + "vw"
+
+        gitIcon.style.top = (sideblockBottom+20) + "px";
+        gitIcon.style.left = 42 + "vw"
+        gitIcon.style.width = 12 + "vw"
+
+        linkedinIcon.style.top = (sideblockBottom+20) + "px";
+        linkedinIcon.style.left = 62 + "vw"
+        linkedinIcon.style.width = 12 + "vw"
+
+    }
+}
+
+window.addEventListener("load", updateStylesForPhone);
+window.addEventListener("resize", updateStylesForPhone);
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to set the active link
+    function setActiveLink() {
+        var sections = document.querySelectorAll('.page');
+        var links = document.querySelectorAll('.navbar ul li a');
+        
+        sections.forEach(function(section) {
+            var rect = section.getBoundingClientRect();
+            if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+                links.forEach(function(link) {
+                    link.classList.remove('active');
+                    if (link.getAttribute('href') === '#' + section.id) {
+                        link.classList.add('active');
+                    }
+                });
+            }
+        });
+    }
+    setActiveLink();
+    window.addEventListener('scroll', setActiveLink);
+});
