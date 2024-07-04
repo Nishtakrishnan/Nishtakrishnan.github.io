98f0a191b3fbd36c178afede5f0faf3b187107b8
Nishta <nishtak2@illinois.edu>
2024-07-05 01:42:26 +0530

    intro page

--- js/pageDimensions.js
+++ js/pageDimensions.js
@@ -2,14 +2,18 @@ function updateDimensionsPages() {
     if (window.innerHeight > window.innerWidth) {
         var scrollButton = document.querySelector(".mail-icon");
         var aboutSection = document.getElementById("about");
+        var experienceSection = document.getElementById("experience");
         var scrollButtonBottom = scrollButton.offsetTop + scrollButton.offsetHeight;
         aboutSection.style.top = (scrollButtonBottom+100) + "px";
+        experienceSection.style.top = (aboutSection.offsetTop + aboutSection.offsetHeight) + "px";
     }
     else {
         var scrollButton = document.querySelector(".mail-icon");
         var aboutSection = document.getElementById("about");
+        var experienceSection = document.getElementById("experience");
         var scrollButtonBottom = scrollButton.offsetTop + scrollButton.offsetHeight;
         aboutSection.style.top = (scrollButtonBottom+150) + "px";
+        experienceSection.style.top = (aboutSection.offsetTop + aboutSection.offsetHeight) + "px";
 
     }
 }
