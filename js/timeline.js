364aa6cc887c934e9d32b7f209ff99526c65472d
Nishta <nishtak2@illinois.edu>
2024-08-28 16:21:48 +0530

    added repos to project page

--- js/timeline.js
+++ js/timeline.js
@@ -15,21 +15,41 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+
 function showNextSlide() {
     const currentSlide = document.querySelector('.stroke-text').innerText;
+    const video = document.querySelector('.placeranker-video');
+    const githubButton = document.querySelector('.github-button');
+    
     if (currentSlide === 'Placeranker') {
         document.querySelector('.stroke-text').innerText = 'Traveling Salesman';
         document.querySelector('.left-arrow').style.display = 'block';
         document.querySelector('.right-arrow').style.display = 'none';
+        video.style.display = 'none';
+        githubButton.classList.remove('github-button');
+        githubButton.classList.add('github-button-traveling-salesman');
+        githubButton.href = 'https://github.com/Nishtakrishnan/TravelingSalesman';
+        githubButton.style.display = 'block';
     }
 }
 
 function showPreviousSlide() {
     const currentSlide = document.querySelector('.stroke-text').innerText;
+    const video = document.querySelector('.placeranker-video');
+    const githubButton = document.querySelector('.github-button-traveling-salesman');
+    
     if (currentSlide === 'Traveling Salesman') {
         document.querySelector('.stroke-text').innerText = 'Placeranker';
         document.querySelector('.left-arrow').style.display = 'none';
         document.querySelector('.right-arrow').style.display = 'block';
+        video.style.display = 'block'; 
+        githubButton.classList.remove('github-button-traveling-salesman');
+        githubButton.classList.add('github-button');
+        githubButton.href = 'https://github.com/Nishtakrishnan/Placeranker';
+        githubButton.style.display = 'block';
     }
 }
+
 
