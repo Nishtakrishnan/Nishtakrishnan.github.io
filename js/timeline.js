7632b12d48564d0366c781991246b50d0c5deed4
Nishta <nishtak2@illinois.edu>
2024-08-28 21:19:11 +0530

    fixed project page

--- js/timeline.js
+++ js/timeline.js
@@ -27,6 +27,9 @@ function showNextSlide() {
         document.querySelector('.stroke-text').innerText = 'Traveling Salesman';
         document.querySelector('.left-arrow').style.display = 'block';
         document.querySelector('.right-arrow').style.display = 'none';
+        document.querySelector('.grey-rectangle').style.display = 'none';
+        document.querySelector('.project-description').style.display = 'none';
+        document.querySelector('.tech-stack').style.display = 'none';
         video.style.display = 'none';
         githubButton.classList.remove('github-button');
         githubButton.classList.add('github-button-traveling-salesman');
@@ -44,6 +47,9 @@ function showPreviousSlide() {
         document.querySelector('.stroke-text').innerText = 'Placeranker';
         document.querySelector('.left-arrow').style.display = 'none';
         document.querySelector('.right-arrow').style.display = 'block';
+        document.querySelector('.grey-rectangle').style.display = 'block';
+        document.querySelector('.project-description').style.display = 'block';
+        document.querySelector('.tech-stack').style.display = 'block';
         video.style.display = 'block'; 
         githubButton.classList.remove('github-button-traveling-salesman');
         githubButton.classList.add('github-button');
